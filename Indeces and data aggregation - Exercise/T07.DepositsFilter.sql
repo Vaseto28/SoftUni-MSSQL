c4a968af3664298f78147f6b1178e254d30c6acc
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-02-15 20:34:31 +0200

    Completed task: T07.DepositsFilter

---
+++ Indeces and data aggregation - Exercise/T07.DepositsFilter.sql
@@ -0,0 +1,5 @@
+SELECT DepositGroup, SUM(DepositAmount) AS LongestMagicWand FROM WizzardDeposits
+WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup
+HAVING SUM(DepositAmount) < 150000
+ORDER BY SUM(DepositAmount) DESC
