9ed85311f99bfd08cafd79b38990c874fcd26494
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-02-20 14:42:36 +0200

    Completed task: T11.AllVolunteersInDepartment

---
+++ Exam preparations/1. First exam preparation - Zoo/T11.AllVolunteersInADepartment.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION udf_GetVolunteersCountFromADepartment (@VolunteersDepartment VARCHAR(30))
+RETURNS INT
+AS
+BEGIN
+    RETURN(
+        SELECT COUNT(*) FROM Volunteers AS v
+        JOIN VolunteersDepartments AS vd ON v.DepartmentId = vd.Id
+        WHERE vd.DepartmentName = @VolunteersDepartment
+    )
+END
+
+SELECT dbo.udf_GetVolunteersCountFromADepartment ('Guest engagement')
