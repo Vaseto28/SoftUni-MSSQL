af97ae7132f6d0abafbeb87b98634cd6a12acd4e
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-02-21 12:25:07 +0200

    Completed task: T06.SitesWithTheirLocationAndCategory

---
+++ Exam preparations/2. Second exam preparation - 100 National Tourist Sites of Bulgaria/T06.SitesWithTheirLocationAndCategory.sql
@@ -0,0 +1,4 @@
+SELECT s.[Name], l.[Name] AS [Location], s.Establishment, c.[Name] AS Category FROM Sites AS s
+JOIN Locations AS l ON s.LocationId = l.Id
+JOIN Categories AS c ON s.CategoryId = c.Id
+ORDER BY c.[Name] DESC, l.[Name], s.[Name]
