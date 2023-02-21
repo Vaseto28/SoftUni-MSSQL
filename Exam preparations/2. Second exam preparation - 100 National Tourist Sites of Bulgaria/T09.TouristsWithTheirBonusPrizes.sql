ea91115d5e8fbe442af8c25ec0147a25cb641ad0
Vasil Kremenarov <vaseto.k28@gmail.com>
2023-02-21 12:26:38 +0200

    Completed task: T09.TouristsWithTheirBonusPrizes

---
+++ Exam preparations/2. Second exam preparation - 100 National Tourist Sites of Bulgaria/T09.TouristsWithTheirBonusPrizes.sql
@@ -0,0 +1,4 @@
+SELECT t.[Name], t.Age, t.PhoneNumber, t.Nationality, ISNULL(bp.[Name], '(no bonus prize)') Reward FROM Tourists AS t
+LEFT JOIN TouristsBonusPrizes AS tbp ON t.Id = tbp.TouristId
+LEFT JOIN BonusPrizes AS bp ON tbp.BonusPrizeId = bp.Id
+ORDER BY t.[Name]
